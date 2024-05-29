d2df917dbc221a47f8a287bc888ca012bac2fd4d
haq5hc <thanhbo025@gmail.com>
2024-05-29 10:39:36 +0700

    Add breakpoint class

---
+++ include/breakpoint.hpp
@@ -0,0 +1,23 @@
+#include <linux/types.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <cstdint>
+
+class breakpoint {
+    public:
+        breakpoint(pid_t pid, std::intptr_t addr)
+            : m_pid{pid}, m_addr{addr}, m_enabled{false}, m_saved_data{}
+        {}
+
+        void enable();
+        void disable();
+
+        auto is_enabled () const -> bool { return m_enabled; }
+        auto get_address() const -> std::intptr_t { return m_addr; }
+
+    private:
+    pid_t m_pid;
+    std::intptr_t m_addr;
+    bool m_enabled;
+    uint8_t m_saved_data;
+};
