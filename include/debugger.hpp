ac746ddd5637954435415691cb146129b9358670
haq5hc <thanhbo025@gmail.com>
2024-05-31 17:20:08 +0700

    Add breakpoints to debugger

--- include/debugger.hpp
+++ include/debugger.hpp
@@ -11,6 +11,7 @@
 #include <utility>
 #include <vector>
 
+#include <breakpoint.hpp>
 #include "linenoise.h"
 
 class debugger {
@@ -21,12 +22,14 @@ class debugger {
         void run();
         void continue_execution();
         void handle_command(const std::string& line);
+        void set_breakpoint_at_address(std::intptr_t addr);
         std::vector<std::string> split(const std::string& s, char delimiter);
         bool is_prefix(const std::string& s, const std::string& of);
 
     private:
         std::string m_prog_name;
         pid_t m_pid;
+        std::unordered_map<std::intptr_t, breakpoint> m_breakpoints;
 };
 
 void debugger::run() {
@@ -44,15 +47,26 @@ void debugger::run() {
 }
 
 void debugger::handle_command(const std::string& line) {
-    auto args = split(line, ' ');
+    auto args = split(line,' ');
     auto command = args[0];
 
-    if (is_prefix(command, "continue")) {
+    if (is_prefix(command, "cont")) {
         continue_execution();
+    }
+    else if(is_prefix(command, "break")) {
+        std::string addr {args[1], 2}; //naively assume that the user has written 0xADDRESS
+        set_breakpoint_at_address(std::stol(addr, 0, 16));
     }
     else {
         std::cerr << "Unknown command\n";
     }
+}
+
+void debugger::set_breakpoint_at_address(std::intptr_t addr) {
+    std::cout << "Set breakpoint at address 0x" << std::hex << addr << std::endl;
+    breakpoint bp {m_pid, addr};
+    bp.enable();
+    m_breakpoints[addr] = bp;
 }
 
 std::vector<std::string> debugger::split(const std::string& s, char delimiter) {
